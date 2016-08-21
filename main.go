fec91e2775cd622819ca302e93f7189b48065868
Stephan van Ellewee <stephan.vanellewee@takealot.com>
2016-08-21 13:35:28 +0200

    added some zmq notes

--- main.go
+++ main.go
@@ -2,10 +2,53 @@ package main
 
 import (
 	"fmt"
-	"github.com/golang/protobuf/proto"
+	// "github.com/golang/protobuf/proto"
+	zmq "github.com/pebbe/zmq4"
+	"os"
 )
 
+func serverCode(responder *zmq.Socket) {
+	msg, _ := responder.Recv(0)
+	fmt.Println("Server RX", msg)
+	result := fmt.Sprintf("CHANGED %s", msg)
+	responder.Send(result, 0)
+}
+
+func runServer(doSomething func(*zmq.Socket)) {
+	responder, _ := zmq.NewSocket(zmq.REP)
+	responder.Bind("tcp://*:5555")
+	defer responder.Close()
+
+	for {
+		doSomething(responder)
+	}
+}
+
+func clientCode(requester *zmq.Socket) {
+	for i := 0; i< 10; i++ {
+		msg := fmt.Sprintf(">>%d>>", i)
+		requester.Send(msg, 0)
+		retval, _ := requester.Recv(0)
+		fmt.Println("Getting ", retval)
+	}
+}
+
+func runClient(doSomething func(*zmq.Socket)){
+	requester, _ := zmq.NewSocket(zmq.REQ)
+	requester.Connect("tcp://localhost:5555")
+	defer requester.Close()
+	doSomething(requester)
+}
+/*
+HOw about we create a new protobuf, and send it via REQ REP?
+*/
 func main() {
-	fmt.Println("Let's get dangerous")
-	fmt.Printf("SSS %s\n\n", proto.String("hello"))
+	arguments := os.Args[1:]
+
+	if len(arguments) > 0 {
+		runServer(serverCode)
+		return
+	}
+	runClient(clientCode)
+	
 }
